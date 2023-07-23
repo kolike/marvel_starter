a86a2fa623276d605dfbebaf0022fbec0f660dce
kolike <kolik_2604@mail.ru>
2023-07-23 15:33:18 +0300

    ref

--- src/components/charListItem/CharListItem.js
+++ src/components/charListItem/CharListItem.js
@@ -1,16 +1,33 @@
-const CharListItem = ({ src, alt, name, onCharSelected }) => {
-  let classNameImg = '';
-  if (src.indexOf('image_not_available') !== -1) {
-    classNameImg = 'char__item_img_not_found';
-  } else {
-    classNameImg = 'char__item';
-  }
+import { Component, createRef } from 'react';
 
-  return (
-    <li className={classNameImg} onClick={onCharSelected}>
-      <img src={src} alt={alt} />
-      <div className="char__name">{name}</div>
-    </li>
-  );
-};
+class CharListItem extends Component {
+  ref = createRef();
+
+  render() {
+    const { src, alt, name, onCharSelected } = this.props;
+    const isFocus = this.ref.current === document.activeElement;
+    console.log(isFocus);
+
+    let classNameImg = '';
+    if (src.indexOf('image_not_available') !== -1) {
+      classNameImg = `char__item_img_not_found ${
+        isFocus ? 'char__item_img_not_found_selected' : ''
+      }`;
+    } else {
+      classNameImg = `char__item ${isFocus ? 'char__item_selected' : ''}`;
+    }
+    return (
+      <li
+        ref={this.ref}
+        className={classNameImg}
+        onClick={onCharSelected}
+        onFocus={onCharSelected}
+        tabIndex={0}
+      >
+        <img src={src} alt={alt} />
+        <div className="char__name">{name}</div>
+      </li>
+    );
+  }
+}
 export default CharListItem;
